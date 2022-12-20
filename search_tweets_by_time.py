594e143b72790ed55eb4bcc8c039f62f182565d4
Meenakshi Pavithran <42751267+mpavithr@users.noreply.github.com>
2022-12-20 14:00:33 -0500

    Update search_tweets_by_time.py

--- search_tweets_by_time.py
+++ search_tweets_by_time.py
@@ -1,1 +1,17 @@
+import tweepy
 
+# Replace with API keys and secrets
+consumer_key = "MY_CONSUMER_KEY"
+consumer_secret = "MY_CONSUMER_SECRET"
+access_key = "MY_ACCESS_KEY"
+access_secret = "MY_ACCESS_SECRET"
+
+# Authenticate with Twitter API
+auth = tweepy.OAuth1UserHandler(consumer_key, consumer_secret, access_key, access_secret)
+api = tweepy.API(auth)
+
+# Search for tweets within the past week
+tweets = api.search(q="", lang="en", since_id="2019-01-01", until_id="2019-01-07")
+
+for tweet in tweets:
+  print(tweet.text)
