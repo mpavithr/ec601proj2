6042dda81ca7cb7a628a50edfe0c04c014e56069
Meenakshi Pavithran <42751267+mpavithr@users.noreply.github.com>
2022-12-20 13:56:51 -0500

    Update retrieving_tweets.py

--- retrieving_tweets.py
+++ retrieving_tweets.py
@@ -1,1 +1,17 @@
+import tweepy
 
+# Have to replace these with API keys and secrets
+consumer_key = "MY_CONSUMER_KEY"
+consumer_secret = "MY_CONSUMER_SECRET"
+access_key = "MY_ACCESS_KEY"
+access_secret = "MY_ACCESS_SECRET"
+
+# Authenticate with Twitter API
+auth = tweepy.OAuth1UserHandler(consumer_key, consumer_secret, access_key, access_secret)
+api = tweepy.API(auth)
+
+# Retrieve tweets from user's home timeline
+tweets = api.home_timeline()
+
+for tweet in tweets:
+  print(tweet.text)
